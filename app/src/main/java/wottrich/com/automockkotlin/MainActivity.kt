dcac9de8ced706a0a89387a108adcf7f5dac904c
wottrich <lucas.wottrich@operacao.rcadigital.com.br>
2019-06-23 02:23:53 -0300

    AutoMock 3.0

--- app/src/main/java/wottrich/com/automockkotlin/MainActivity.kt
+++ app/src/main/java/wottrich/com/automockkotlin/MainActivity.kt
@@ -6,17 +6,7 @@ import android.widget.Toast
 import wottrich.com.mock_annotations.MockField
 import wottrich.com.mock_annotations.MockModel
 
-@MockModel(customName = "CustomerModel", list = true, body = "{\"name\":\"Lucas\", \"age\":34}")
 open class MainActivity : AppCompatActivity() {
-
-    // body = "{\"name\":\"Lucas\", \"age\":34}"
-
-    //@MockField(String::class, "Lucas", "name")
-    //private var name: String? = null
-
-    //@MockField(Int::class, "10", "age")
-    //private var age: Int? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
